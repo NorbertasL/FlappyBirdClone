d944dc16a39c426314f076177879521c1a4f7af0
Red_Spark <redsparkdev@gmail.com>
2017-01-19 11:06:04 +0000

    Initial commit

---
+++ desktop/src/com/redspark/flappybirdclone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redspark.flappybirdclone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redspark.flappybirdclone.FlappyBirdClone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBirdClone(), config);
+	}
+}
